0305504d4c599fd9289ad8739c6397cd850729b2
Eynar J. Alvarez M <115285455+Eynar92@users.noreply.github.com>
2024-02-14 02:08:36 -0400

    Actualizar index.jsx

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -32,7 +32,7 @@ export const router = createBrowserRouter([
         element: <LoginPage />
     },
     {
-        path: "/*",
+        path: "*",
         element: <Navigate to="/marvel" />
     },
 ])
